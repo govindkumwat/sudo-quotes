40eec4a9fc17387022297a2efb3d6dbff7a7140d
govindkumwat <37894080+govindkumwat@users.noreply.github.com>
2021-06-09 11:14:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    function fetchData() {
+        fetch('https://goquotes-api.herokuapp.com/api/v1/random?count=999')
+        .then(resp => resp.json())
+        .then(data => renderQuotes(data.quotes))
+      }
+      function renderQuotes(data) {
+          for (const q of data) {
+
+          const quoteUL = document.querySelector('#quote-list');
+    
+          const quoteLi = document.createElement('li');
+          const blockQuote = document.createElement('blockquote');
+          const p = document.createElement('p');
+          const footer = document.createElement('footer');
+          const br = document.createElement('br');
+          const hr = document.createElement('hr')
+   
+          quoteLi.className = 'quote-card';          
+          blockQuote.className = 'blockquote';       
+          p.className = 'mb-0';                      
+          footer.className = 'blockquote-footer';    
+
+    
+          p.innerHTML = q.text;
+          footer.innerHTML = q.author;
+        
+    
+          blockQuote.append(p, footer, br, hr);
+          quoteLi.append(blockQuote);
+          quoteUL.append(quoteLi);
+          }
+       }
+    
+       fetchData();
+    })
